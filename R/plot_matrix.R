74b79c7b74a179798787b2ebae18eb45f886c368
Juliette Chiapello <chiapellojuliette@gmail.com>
2021-06-05 15:50:35 +0200

    No more warnings

--- R/plot_matrix.R
+++ R/plot_matrix.R
@@ -16,21 +16,20 @@
 #'
 #' plot_matrix(matrix(rbinom(10*10, 1, 0.5), nrow = 10))
 plot_matrix <- function(M, r= "", paletteinf = "", palettesup = "", titre = "", nom_axeX = "", nom_axeY = "", echelle = "Echelle"){
-  titre <- stringr::str_c(titre, "  Var = " ,as.character(round(variance(M), 4)),  sep = "")
-  M <- reshape2::melt(M) # trois colonnes : les i, les j, les valeurs
-  p <- ggplot2::ggplot(data = M, ggplot2::aes(x = Var1 -0.5 , y = Var2-0.5) )
-  if(r != ""){p <- ggplot2::ggplot(M, ggplot2::aes(x = Var1 -0.5+r , y = Var2 -0.5+r, z= value, fill=value))}
+  titre <- str_c(titre, "   Var = " ,as.character(round(variance(M), 4)),  sep = "")
+  M <- long(M) # trois colonnes : les i, les j, les valeurs
+  p <- ggplot(data = M, aes(x = Var1 -0.5, y = Var2 - 0.5) )
+  if(r != ""){p <- ggplot(M, aes(x = Var1 -0.5+r , y = Var2 -0.5+r, z= value, fill=value))}
   p <- p +
-    ggplot2::geom_tile(ggplot2::aes(fill = value))
-  if(paletteinf != "" & palettesup != ""){p <- p+ggplot2::scale_fill_viridis_c(option = "B", direction = -1, limits = c(paletteinf,palettesup))}
-  else{p <- p+ggplot2::scale_fill_viridis_c(option = "B", direction = -1)}
+    geom_tile(aes(fill = value))
+  if(paletteinf != "" & palettesup != ""){p <- p+scale_fill_viridis_c(option = "B", direction = -1, limits = c(paletteinf, palettesup))}
+  else{p <- p+scale_fill_viridis_c(option = "B", direction = -1)}
   p <- p +
-    ggplot2::scale_y_reverse() +
-    ggplot2::labs(title = titre,
+    labs(title = titre,
          x = nom_axeX,
          y = nom_axeY) +
-    ggplot2::guides(fill = ggplot2::guide_colorbar(title = echelle))+
-    ggplot2::scale_y_continuous(breaks= scales::pretty_breaks())+
-    ggplot2::scale_x_continuous(breaks= scales::pretty_breaks())
+    guides(fill = guide_colorbar(title = echelle))+
+    scale_y_continuous(breaks= pretty_breaks())+
+    scale_x_continuous(breaks= pretty_breaks())
   return(p)
 }
