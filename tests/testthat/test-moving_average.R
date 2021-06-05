2251bd279534b260e499e89228d8a5a538ea7365
Juliette Chiapello <chiapellojuliette@gmail.com>
2021-06-05 17:15:29 +0200

    Replace error messages by true errors (stop), added unit tests

--- tests/testthat/test-moving_average.R
+++ tests/testthat/test-moving_average.R
@@ -1,10 +1,42 @@
-test_that("moving average works", {
+######################### REGULAR TESTS #######################
+
+
+test_that("Regular test 1", {
+  # arrange
   set.seed(1)
   Z <- matrix(rbinom(5*5, 1, 0.5), nrow = 5)
-  expect_identical(moving_average(Z,2), matrix(c(13/25)))
+  # act
+  actual <- moving_average(Z,2)
+  # assert
+  expected <- matrix(c(13/25))
+  expect_identical(actual, expected)
 })
 
 
+
+######################## ANOMALY TESTS #####################
+
+
+test_that("Anomaly test - error when r parametre isn't a number", {
+  # arrange
+  Z <- matrix(rbinom(5*5, 1, 0.5), nrow = 5)
+  # act & assert
+  expect_error(moving_average(Z, "a"), "^r must be an integer$")
+})
+
+test_that("Anomaly test - error when r parametre is a floating number", {
+  # arrange
+  Z <- matrix(rbinom(5*5, 1, 0.5), nrow = 5)
+  # act & assert
+  expect_error(moving_average(Z, 0.5), "^r must be an integer$")
+})
+
+test_that("Anomaly test - error when r parametre is less than zero", {
+  # arrange
+  Z <- matrix(rbinom(5*5, 1, 0.5), nrow = 5)
+  # act & assert
+  expect_error(moving_average(Z, -2), "^r must be >= 0$")
+})
 
 
 
